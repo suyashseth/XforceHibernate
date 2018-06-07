b5bbd9ede9ef994c88bfe744b323d5c31cc7ce43
suyash <suyash@suyash-PC>
2018-06-07 15:15:06 +0530

    commit

--- src/main/java/com/dev/dao/UserInfoDAO.java
+++ src/main/java/com/dev/dao/UserInfoDAO.java
@@ -6,6 +6,6 @@ public interface UserInfoDAO {
 
 	boolean  CreateProfile(UserInfo user);
 	UserInfo SearchStudent(int id,String pass);
-	boolean UpdatePassword(int id,String password,String npass, String npass2);
+	boolean UpdatePassword(int id,String  password,String npass, String npass2);
 	boolean DeleteUser(int id,String password);
 }
--- src/main/java/com/dev/test/UserOperation.java
+++ src/main/java/com/dev/test/UserOperation.java
@@ -3,8 +3,9 @@ package com.dev.test;
 public class UserOperation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
